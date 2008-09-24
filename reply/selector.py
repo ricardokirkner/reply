42021e743db00b7e3c35791cf7d3d7d3e65e0f3d
ricardokirkner <ricardokirkner@localhost>
2008-09-24 03:13:34 +0000

    added SoftMaxSelector class

--- reply/selector.py
+++ reply/selector.py
@@ -1,6 +1,7 @@
 import numpy
 import random
 
+
 class Selector(object):
     def new_episode(self):
         """
@@ -16,7 +17,8 @@ class Selector(object):
         The parameters are received in rl-encoding
         """
         raise NotImplementedError()
-        
+
+
 class EGreedySelector(Selector):
     def __init__(self, epsilon, decay=1):
         self.epsilon = epsilon
@@ -35,4 +37,32 @@ class EGreedySelector(Selector):
             action = numpy.argmax( action_value_array )
         #print action
         return action
-    
+
+
+class SoftMaxSelector(Selector):
+    def __init__(self, temperature):
+        self.temperature = temperature
+
+    def select_action(self, encoded_state):
+        action_value_array = self.rl.storage.get_state_values(encoded_state)
+        if self.temperature == 0:
+            # this should be absolute greedy selection
+            action = numpy.argmax(action_value_array)
+        else:
+            # get all actions for this state, and their values
+            # select a probability
+            pr = random.random()
+            # get the total value
+            temperature = self.temperature
+            total_value = sum([numpy.exp(value/temperature) for value in action_value_array])
+            # select the softmax action
+            current_pr = 0
+            for action,value in enumerate(action_value_array):
+                # get the action probability
+                action_pr = numpy.exp(value/temperature) / total_value
+                # total all past action probabilities
+                current_pr += action_pr
+                if pr < current_pr:
+                    break
+        return action
+
