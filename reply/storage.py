c5d398cac0d47e850e050804aa7edcea424e1eb1
lucio.torre <lucio.torre@localhost>
2008-09-28 18:43:49 +0000

    cambios

--- reply/storage.py
+++ reply/storage.py
@@ -102,6 +102,42 @@ class DebugTableStorage(TableStorage):
         self.debug_state = numpy.zeros( (encoder.input_size, encoder.output_size) )
 
     def store_value(self, state, action, new_value):
+        self.value_change += abs(new_value-self.state[state, action])
         super(DebugTableStorage, self).store_value(state, action, new_value)
+        self.sum_state_action_visits += self.debug_state[state, action]
+        self.sum_state_visits += sum(self.debug_state[state])
+        if sum(self.debug_state[state]) == 0:
+            self.new_state_visits += 1
+        if self.debug_state[state, action] == 0:
+            self.new_state_action_visits += 1
+        self.total_visits += 1
         self.debug_state[state, action]+=1
+
+    def print_report(self):
+        shape = self.debug_state.shape
+        print "Dimension usage variation"
+        print "Dimension usage median"
+        print "Dimension usage average"
+        print "Dimension coverage"
+        print "State Coverage", len(numpy.nonzero(numpy.sum(self.debug_state,1)))/float(shape[0])
+        #print self.debug_state
+        print "State Coverage median", numpy.median(numpy.sum(self.debug_state, 1))
+        sz = shape[0]*shape[1]
+        print "State-Action Coverage", len(numpy.nonzero(self.debug_state.reshape(sz)))/float(sz)
+        print "State-Action Coverage median", numpy.median(
+                                                numpy.median(self.debug_state))
+        print "new states visited:", self.new_state_visits, "%0.2f%%"%(100*self.new_state_visits/float(self.total_visits))
+        print "new actions taken:", self.new_state_action_visits, "%0.2f%%"%(100*self.new_state_action_visits/float(self.total_visits))
+        print "sum state visit", self.sum_state_visits, "%0.2f"%(self.sum_state_visits/self.total_visits)
+        print "sum state action visit", self.sum_state_action_visits, "%0.2f"%(self.sum_state_action_visits/self.total_visits)
+        print "value change", self.value_change, self.value_change/float(self.total_visits)
+
+    def new_episode(self):
+        super(DebugTableStorage, self).new_episode()
+        self.new_state_visits = 0
+        self.sum_state_visits = 0
+        self.new_state_action_visits = 0
+        self.sum_state_action_visits = 0
+        self.total_visits = 0
+        self.value_change = 0
 
