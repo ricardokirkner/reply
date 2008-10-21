f082d4880bb1ce4d9078e33336a4b73eb465d0a8
ricardokirkner <ricardokirkner@localhost>
2008-10-21 17:56:23 +0000

    first version of gridworld working again.

--- samples/gridworld.py
+++ samples/gridworld.py
@@ -13,37 +13,21 @@ debug = 0
 class GridWorld(reply.World):
     ENTER, EXIT, MINE = range(3)
     
-    def __init__(self, size, enter, exits, mines):
+    def __init__(self, rl):
         """
         size: tuple for the world size
         enter: tuple coordinate for the point of enter
         exit: tuple coordinate for the exit points
         mines: list of tuple coordinates for the mines positions
         """
-        self.size = size
-        self.enter = enter
-        self.exits = exits
-        self.mines = mines
+        super(GridWorld, self).__init__(rl)
+        self.size = getattr(rl, 'size')
+        self.enter = getattr(rl, 'enter')
+        self.exits = getattr(rl, 'exits')
+        self.mines = getattr(rl, 'mines')
         
-        self.position = self.enter
         
-
-    def get_action_space(self):
-        return [ reply.dimension(0,3,4)  ]
-    
-    def get_state_space(self):
-        xz, yz = self.size
-        return [ reply.dimension(0,xz-1,xz), reply.dimension(0,yz-1,yz), ]
-        
-    def get_reward(self, state):
-        if self.position in self.exits:
-            return 10
-        if self.position in self.mines:
-            return -1
-        return -0.01
-    
-        
-    def is_final(self, state):
+    def is_final(self):
         if self.position in self.exits:
             return True
         return False
@@ -59,7 +43,7 @@ class GridWorld(reply.World):
             p = (position[0]+1)%self.size[0], position[1]
         return p
     
-    def do_action(self, solver, action):
+    def do_action(self, action):
         dir = action[0]
         if debug:
             print "at", self.position,
@@ -75,78 +59,102 @@ class GridWorld(reply.World):
             else:
                 print
         
-    
-            
-        
-        
-    def get_initial_state(self):
+    def get_state(self):
+        x, y = self.position
+        return dict(
+                x=x,
+                y=y
+                )
+
+    def new_episode(self):
         self.trace = [self.enter]
         self.position = self.enter
-        return self.get_state()
-    
-    def get_state(self):
-        return self.position
         
+
+class GridWorldAgent(reply.LearningAgent):
+    learning_rate = 1
+    learning_rate_decay = 0.99
+    learning_rate_min = 0.05
+    value_discount = 0.01
+
+    random_action_rate = 0.1
+    random_action_rate_decay = 1
+
+    size = (10,10)
+    enter = (1,1)
+    exits = [(5,5)]
+    mines = [(0,0), (1,0), (2,0),
+             (0,1),
+             (0,2), (1,2), (2,2),
+             (3,4), (4,4), (5,4), (6,4),
+             (3,5),
+             (3,6), (4,6), (5,6), (6,6)]
+
+    world_class = GridWorld
+    learner_class = reply.learner.QLearner
+    selector_class = reply.selector.EGreedySelector
+    storage_class = reply.storage.TableStorage
+    encoder_class = reply.encoder.DistanceEncoder
+
+    def get_action_space(self):
+        return [ reply.Dimension('action',0,3)  ]
     
+    def get_state_space(self):
+        xz, yz = self.world.size
+        return [ reply.Dimension('x',0,xz-1), reply.Dimension('y',0,yz-1), ]
+        
+    def get_reward(self):
+        if self.world.position in self.world.exits:
+            return 10
+        if self.world.position in self.world.mines:
+            return -1
+        return -0.01
+
+
 if __name__ == "__main__":
     import pyglet
     from pyglet import gl
-    
-    g = GridWorld((10,10), (1,1), [(5,5)], 
-                  [
-                   (0,0), (1,0), (2,0), 
-                   (0,1),
-                   (0,2), (1,2), (2,2), 
-                   (3,4), (4,4), (5,4), (6,4),
-                   (3,5),
-                   (3,6), (4,6), (5,6), (6,6)
-               ])
-    space = g.get_state_space(), g.get_action_space()
-    encoder = reply.encoder.DistanceEncoder(*space)
-    storage = reply.storage.TableStorage(encoder)
-    #learner = reply.learner.QLearner(1, 0.01,0.99, 0.05)
-    learner = reply.learner.SarsaLearner(1, 0.01,0.99, 0.05)
-    selector = reply.selector.EGreedySelector(0.1, 1)
-    
-    r = reply.RL(
-            learner, 
-            storage,
-            encoder, 
-            selector
-        )
+
+    # Agent
+    agent = GridWorldAgent()
+
+    # Viewer
     delta = 0
     cellsize = 24 + delta * 2
-    sx, sy = g.size
-    window = pyglet.window.Window(width=max(200, sx*cellsize), height=sy*cellsize+100)
+    sx, sy = agent.world.size
+
+    class Mouse: pass
+    mouse = Mouse()
+    mouse.x = mouse.y = 0
+
+    window = pyglet.window.Window(width=max(200, sx*cellsize), 
+                                  height=sy*cellsize+100)
+
     up = pyglet.resource.image('up.png')
     down = pyglet.resource.image('down.png')
     left = pyglet.resource.image('left.png')
     right = pyglet.resource.image('right.png')
     exit = pyglet.resource.image('exit.png')
     mine = pyglet.resource.image('mine.png')
-    
 
-    class Mouse: pass
-    mouse = Mouse()
-    mouse.x = mouse.y = 0
     @window.event
     def on_mouse_motion(x, y, dx, dy):
         mouse.x = x
         mouse.y = y
-        
+            
     def get_path():
-        current = g.enter
+        current = agent.world.enter
         visit = {}
         path = []
         while not current in visit:
             path.append( current )
             visit[current]=1
-            current = g.move( current, numpy.argmax(storage.get_state_values( encoder.encode_state( current ) )) )
-            if current in g.exits:
+            current = agent.world.move( current, numpy.argmax(agent.storage.get_state_values( agent.encoder.encode_state( dict(x=current[0], y=current[1]) ) )) )
+            if current in agent.world.exits:
                 path.append(current)
                 break
         return path
-            
+                
     @window.event
     def on_draw():
         window.clear()
@@ -158,29 +166,28 @@ if __name__ == "__main__":
         gl.glVertex2f( window.width, window.height )
         gl.glVertex2f( window.width, 0 )
         gl.glEnd()
-        
-
+    
         # draw cell values
         x = mouse.x // cellsize
         y = mouse.y // cellsize
         if x < sx and y < sy:
-            vals = storage.get_state_values( encoder.encode_state( (x,y) ) )
+            vals = agent.storage.get_state_values( agent.encoder.encode_state( dict(x=x, y=y) ) )
             label = pyglet.text.HTMLLabel(
                 '<pre>cell %i,%i:\nup   : %e\ndown : %e\nleft : %e\nright: %e</font></pre>'%(x,y, vals[0], vals[1], vals[2], vals[3]),
                           x=0, y=window.height,multiline=True, width=window.width, anchor_y='top'
                           )
             label.draw()
-
+   
         # draw cell direction
         for x in range(sx):
             for y in range(sy):
-                if (x,y) in g.mines:
+                if (x,y) in agent.world.mines:
                     img = mine
-                elif (x,y) in g.exits:
+                elif (x,y) in agent.world.exits:
                     img = exit
                 else:
                     img = [up, down, left, right][
-                        numpy.argmax(storage.get_state_values( encoder.encode_state( (x,y) ) ))
+                        numpy.argmax(agent.storage.get_state_values( agent.encoder.encode_state( dict(x=x, y=y) ) ))
                         ]
                 img.blit(x*(cellsize)+delta, y*cellsize+delta)
                 
@@ -194,17 +201,10 @@ if __name__ == "__main__":
         gl.glEnd()
         gl.glColor4f(1,1,1,1) 
 
-            
-            
-    episode = 0
     def step(dt):
-        global episode
-        total_reward,steps  = r.run(g)
-        err = 0
-        print 'Espisode: ',r.episodes,'  Steps:',steps,'  Reward:',total_reward, "alpha", learner.alpha
-        episode += 1
+        episode = agent.run()
+        print episode
 
-    
-    
     pyglet.clock.schedule_interval(step, 0.01)
     pyglet.app.run()
+
