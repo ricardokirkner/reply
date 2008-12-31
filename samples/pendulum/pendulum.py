ae20ab1962359638d7ff06f1531de62ad0adbb54
lucio.torre <lucio.torre@localhost>
2008-12-31 02:47:12 +0000

    pendulum working with new refactor. now visuals.

--- samples/pendulum/pendulum.py
+++ samples/pendulum/pendulum.py
@@ -7,9 +7,6 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), '../..'))
 import random, cPickle
 import math
 
-from pyglet import window
-from pyglet import clock
-from pyglet.gl import *
 
 import pymunk._chipmunk as cp
 import pymunk.util as u
@@ -17,9 +14,6 @@ from pymunk import Vec2d as vec2d
 import ctypes
 
 from numpy import arange, array, size
-import matplotlib
-matplotlib.use("cairo")
-import matplotlib.pyplot
 import numpy
 
 
@@ -108,7 +102,6 @@ class Box:
         shape.contents.u = 0.5
         cp.cpSpaceAddShape(space, shape)
 
-
     def render(self):
         p = self.body.contents.p
         body = self.shape.contents.body
@@ -132,132 +125,67 @@ class Box:
         return "<box %s>"%self.body.contents.p
 
 
-class Pendulum(reply.World):
+class PendulumWorld(reply.World):
 
-    def __init__(self, filename):
-        alpha = 1
-        gamma = 0.6
-        alpha_decay = 0.99
-        min_alpha = 0.05
-        epsilon = 0.9
-        epsilon_decay = 0.99
-        min_epsilon = 0.05
-        self.filename = filename
-        try:
-            pend = cPickle.load(open(filename))
-        except IOError:
-            e = reply.encoder.DistanceEncoder(
-                self.get_state_space(), self.get_action_space()
-                )
-            pend = reply.RL(
-                    reply.learner.QLearner(alpha, gamma, alpha_decay, min_alpha),
-                    reply.storage.DebugTableStorage(e),
-                    e,
-                    reply.selector.EGreedySelector(epsilon, epsilon_decay, min_epsilon)
-                )
-        self.rl = pend
-        clock.set_fps_limit(30)
-        self.win = window.Window(width=800, height=600)
-        self.figure = None
-
+    def __init__(self, rl):
+        super(PendulumWorld, self).__init__(rl)
         cp.cpInitChipmunk()
 
-        #PID stuff
-        self.integral = 0
-        self.last_error = 0
-        self.ki = 0.5
-        self.kd = -0.5
-        self.kp = 1.0
-
-
-
-    def get_action_space(self):
-        return [reply.dimension(-1,1,7)]
-
-    def get_state_space(self):
-        return [
-            # ball relative x position
-            reply.dimension(-84,84,6),
-            # base velocity
-            reply.dimension(-100,100,4),
-            # ball x velocity
-            reply.dimension(-100,100,2),
-            # ball y velocity
-            reply.dimension(-100,100,6),
-        ]
-
-    def get_reward(self, s ):
-        died = 0
-        if self.ball.body.contents.p.y < 100:
-            return -1
-        else:
-            return 0
-        if self.ball.body.contents.p.y < 100:
-            died = -1000
-        return (self.ball.body.contents.p.y-100 + died)
-
-    def is_final(self, state):
+    def is_final(self):
         if self.ball.body.contents.p.y < 100:
             return True
         return False
 
     def get_state(self):
-        return [
-            self.ball.body.contents.p.x-self.base.body.contents.p.x,
-            self.base.body.contents.v.x,
-            self.ball.body.contents.v.x,
-            self.ball.body.contents.v.y,
-            ]
+        return dict(
+            ball_angle=self.ball_angle(),
+            ball_angular_v=self.ball_angle() - self.last_angle,
+            base_vx=self.base.body.contents.v.x
+            )
 
-    def do_action(self, solver, action):
-        # returns new state
-        #print "DO:", action
+    def do_action(self, action):
         action = action[0]
-        cp.cpBodyApplyForce(self.base.body, vec2d(action*10000,0), vec2d(0,0))
-        cp.cpSpaceStep(self.space, dt+random.random()*1.0/100.0)
+        self.save_angle()
+        cp.cpBodyApplyForce(self.base.body, vec2d(action*15000,0), vec2d(0,0))
+        cp.cpSpaceStep(self.space, dt)
         cp.cpBodyResetForces(self.base.body)
 
-        clock.tick()
-        self.win.dispatch_events()
-
-        self.win.clear()
-
-        if self.figure: self.figure.blit(0,0)
-
-        self.ball.render()
-        self.floor.render()
-        self.base.render()
-
-        self.win.flip()
-
-
-    def get_initial_state(self):
+    def new_episode(self):
         # return first state
         self.space = space = cp.cpSpaceNew()
         space.contents.gravity = vec2d(0.0, -900.0)
-
+        space.contents.damping = 0.01
+        
         cp.cpSpaceResizeStaticHash(space, 50.0, 200)
         cp.cpSpaceResizeActiveHash(space, 50.0, 10)
 
         self.ball = ball = Ball(space, 5, 10, pos=(310, 180))
-        self.floor = Wall(space, (-6000,40), (6000,40))
+        self.floor = Wall(space, (-60000,40), (60000,40))
         self.base = base = Box(space, 5, 50, 20, (300,100))
         cp.cpResetShapeIdCounter()
         joint = cp.cpPinJointNew(
             ball.body, base.body, vec2d(0,0), vec2d(0,0)
             )
         cp.cpSpaceAddJoint(space, joint)
-        if self.rl.episodes%100==0:
-            self.save()
-        if False: #self.rl.episodes%10==0:
-            self.integral = 0
-            self.last_error = 0
-            f = matplotlib.pyplot.figure()
-            matplotlib.pyplot.matshow(self.rl.storage.debug_state, fignum=f.number,
-                                      aspect=0.05)
-            f.savefig("debug.png")
-            self.figure = pyglet.image.load("debug.png")
-        return self.get_state()
+        self.save_angle()
+        
+    def ball_angle(self):
+        x = self.ball.body.contents.p.x
+        y = self.ball.body.contents.p.y
+        cx = self.base.body.contents.p.x
+        cy = self.base.body.contents.p.y
+
+        x = x-cx
+        y = y-cy
+        if y == 0: return 0
+        return math.atan( x/y )
+
+    def save_angle(self):
+        self.last_angle = self.ball_angle()
+        
+    def end_episode(self):
+        #self.space.cpDestroy()
+        pass
 
     def save(self):
         cPickle.dump(self.rl, open(self.filename, "w"))
@@ -265,64 +193,50 @@ class Pendulum(reply.World):
     def cleanUp(self):
         cp.cpSpaceFree( self.space )
 
-    def learn(self, iterations, max_steps=10000):
-        try:
-            for episode in xrange(iterations):
-                self.rl.new_episode(self)
-                for step in range(max_steps):
-                    if not self.rl.step(self):
-                        break
-                print ('Espisode: ',episode,'  Steps:',step,
-                        '  Reward:',self.rl.total_reward,
-                        'alpha', self.rl.learner.alpha,
-                        'epsilon', self.rl.selector.epsilon,
-                        "avg_hist", self.rl.storage.median_hits,
-                        "count_hits", self.rl.storage.count_hits)
-        finally:
-            self.save()
 
-    def run_pid(self):
-        while True:
-            ended = False
-            self.get_initial_state()
-            while not ended:
-                state = self.get_state()
-                m = self.get_pid( state )
-                self.do_action(None, [m])
-                ended = self.is_final( state )
-            self.cleanUp()
+class PendulumAgent(reply.Agent):
+    random_action_rate = 0.05
 
-    def get_pid(self, state):
-        e = self.pid_error(state)
+    world_class = PendulumWorld
+    selector_class = reply.selector.EGreedySelector
+    storage_class = reply.storage.TableStorage
+    encoder_class = reply.encoder.DistanceEncoder
 
-        d = 1./5
-        self.integral = self.integral*(1-d) + e*d
-        derivative = e - self.last_error
-        proportional = e
-        self.last_error = e
-        f = self.ki * self.integral + self.kd * derivative + self.kp * proportional
-        print e, f, self.integral
-        print self.get_state()
-        return f
+    learning_rate = 0.3
+    learning_rate_decay = 0.999
+    learning_rate_min = 0.005
+    value_discount = 0.5
 
-    def pid_error(self, state):
-        sz = 100
-        if abs( state[1]/sz ) > 1:
-            return -1
-        return -( math.acos( state[1]/sz ) - math.pi/2)
+    random_action_rate = 0.99
+    random_action_rate_decay = 0.99
+    random_action_rate_min = 0.005
 
+    learner_class = reply.learner.QLearner
+    
+    def get_action_space(self):
+        return [reply.Dimension("push", -1,1,17)]
 
-def run(maxepisodes):
-    p = Pendulum("pendulum.pkl")
-    p.learn(maxepisodes)
+    def get_state_space(self):
+        return [
+            # ball angle
+            reply.Dimension("ball_angle", -math.pi/2,math.pi/2,11),
+            # ball angular velocity
+            reply.Dimension("ball_angular_v", -math.pi/16,math.pi/16,11),
+            # base velocity
+            reply.Dimension("base_vx", -100,100,11),
+        ]
 
-
-if __name__ == '__main__':
-    import sys
-    if len(sys.argv) > 1:
-        p = Pendulum("foo")
-        p.run_pid()
-    else:
-        run(1000000)
+    def get_reward(self):
+        if self.world.ball.body.contents.p.y < 100:
+            return -1000
+        if abs(self.world.ball_angle()) < math.pi/16:
+            return math.pi/16 - abs(self.world.ball_angle())
+        return 0
+    
+    
+if __name__ == "__main__":
+    agent = PendulumAgent()
+    experiment = reply.Experiment(agent)
+    experiment.run()
 
 
