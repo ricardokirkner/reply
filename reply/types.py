7e7bb36fd003f96c39c333ed3c93c55158faa522
ricardokirkner <ricardokirkner@localhost>
2009-05-23 23:07:05 +0000

    started with unit tests for Space

--- reply/types.py
+++ reply/types.py
@@ -8,6 +8,9 @@ class Number(Parameter):
         self.min = min
         self.max = max
 
+    def __eq__(self, other):
+        return (self.min == other.min and self.max == other.max)
+
 
 class Integer(Number):
     def __str__(self):
@@ -30,75 +33,91 @@ class ListOf(Parameter):
 
 
 class Space(object):
-    def __init__(self, spec, order=None):
+    def __init__(self, spec):
         self.spec = spec
-        self.order = order
+        self._data = {}
 
-        self.names, self.values = self._build()
+        self._build_data(spec)
 
-    def _build(self):
-        doubles = []
-        chars = []
-        ints = []
-        spec = self.spec
+    def __getitem__(self, item):
+        return self._data[item]
 
-        if self.order is None:
-            order = sorted(self.spec.keys())
-        else:
-            order = self.order
+    def _build_data(self, spec):
+        for name, value in spec.iteritems():
+            _type = type(value)
+            values = self._data.setdefault(_type, {})
+            values[name] = value
 
-        for key in order:
-            value = spec[key]
-
-            if isinstance(value, ListOf):
-                type_ = value.type
-            else:
-                type_ = value
-
-            if isinstance(type_, Double) :
-                doubles.append((key, value))
-            elif isinstance(type_, Integer):
-                ints.append((key, value))
-            elif isinstance(type_, Char):
-                chars.append((key, value))
-
-        charcount = sum([ x.num if isinstance(x, ListOf) else 1 for x in chars])
-        names = []
-        values = []
-        if ints:
-            values.append("INTS %s" % " ".join([ str(y) for (x, y) in ints]))
-            names.append("INTS %s" % " ".join([ str(x) for (x, y) in ints]))
-        if doubles:
-            values.append("DOUBLES %s" %
-                          " ".join([ str(y) for (x, y) in doubles]))
-            names.append("DOUBLES %s" %
-                         " ".join([ str(x) for (x, y) in doubles]))
-        if chars:
-            values.append("CHARCOUNT %s" % charcount)
-        return (" ".join(names), " ".join(values))
-
-    @classmethod
-    def parse(cls, values):
-        space = Space({})
-        int_count = 0
-        double_count = 0
-        charcount_count = 0
-        for i, val in enumerate(values):
-            if val == 'INTS':
-                int_count += 1
-                ints = values[i+1] + ', ' + values[i+2]
-                int_t = eval(ints)
-                space.spec['int' + str(int_count)] = Integer(*int_t)
-            elif val == 'DOUBLES':
-                double_count += 1
-                doubles = values[i+1] + ', ' + values[i+2]
-                double_t = eval(doubles)
-                space.spec['double' + str(double_count)] = Double(*double_t)
-            elif val == 'CHARCOUNT':
-                charcount = int(values[i+1])
-                for j in xrange(1, charcount+1):
-                    space.spec['char' + str(j)] = Char()
-
-        return space
+#class Space(object):
+#    def __init__(self, spec, order=None):
+#        self.spec = spec
+#        self.order = order
+#
+##        self.names, self.values = self._build()
+#
+#    def _build(self):
+#        doubles = []
+#        chars = []
+#        ints = []
+#        spec = self.spec
+#
+#        if self.order is None:
+#            order = sorted(self.spec.keys())
+#        else:
+#            order = self.order
+#
+#        for key in order:
+#            value = spec[key]
+#
+#            if isinstance(value, ListOf):
+#                type_ = value.type
+#            else:
+#                type_ = value
+#
+#            if isinstance(type_, Double) :
+#                doubles.append((key, value))
+#            elif isinstance(type_, Integer):
+#                ints.append((key, value))
+#            elif isinstance(type_, Char):
+#                chars.append((key, value))
+#
+#        charcount = sum([ x.num if isinstance(x, ListOf) else 1 for x in chars])
+#        names = []
+#        values = []
+#        if ints:
+#            values.append("INTS %s" % " ".join([ str(y) for (x, y) in ints]))
+#            names.append("INTS %s" % " ".join([ str(x) for (x, y) in ints]))
+#        if doubles:
+#            values.append("DOUBLES %s" %
+#                          " ".join([ str(y) for (x, y) in doubles]))
+#            names.append("DOUBLES %s" %
+#                         " ".join([ str(x) for (x, y) in doubles]))
+#        if chars:
+#            values.append("CHARCOUNT %s" % charcount)
+#        return (" ".join(names), " ".join(values))
+#
+#    @classmethod
+#    def parse(cls, values):
+#        space = Space({})
+#        int_count = 0
+#        double_count = 0
+#        charcount_count = 0
+#        for i, val in enumerate(values):
+#            if val == 'INTS':
+#                int_count += 1
+#                ints = values[i+1] + ', ' + values[i+2]
+#                int_t = eval(ints)
+#                space.spec['int' + str(int_count)] = Integer(*int_t)
+#            elif val == 'DOUBLES':
+#                double_count += 1
+#                doubles = values[i+1] + ', ' + values[i+2]
+#                double_t = eval(doubles)
+#                space.spec['double' + str(double_count)] = Double(*double_t)
+#            elif val == 'CHARCOUNT':
+#                charcount = int(values[i+1])
+#                for j in xrange(1, charcount+1):
+#                    space.spec['char' + str(j)] = Char()
+#
+#        return space
 
 
