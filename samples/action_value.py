ff8d34b5eb33b05fb8d9141cf092a36af5d1632e
ricardokirkner <ricardokirkner@localhost>
2009-06-08 01:32:34 +0000

    extended action_value sample to use policy, encoder and storage classes

--- samples/action_value.py
+++ samples/action_value.py
@@ -7,28 +7,57 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 import random
 
 from reply.agent import Agent
+from reply.datatypes import Integer
+from reply.encoder import SpaceEncoder, StateActionEncoder
 from reply.environment import Environment
 from reply.experiment import Experiment
-from reply.datatypes import Integer
+from reply.policy import EGreedyPolicy
+from reply.storage import TableStorage
 
 
 class ActionValueAgent(Agent):
     def _init(self, task_spec):
+        learning_rate = 1
+        learning_rate_decay = 0.99
+        learning_rate_min = 0.001
+        random_action_rate = 1
+
+        #self.learner = reply.learner.QLearner(None)
+        state_encoder = SpaceEncoder(self._observation_space)
+        action_encoder = SpaceEncoder(self._action_space)
+        encoder = StateActionEncoder(state_encoder, action_encoder)
+        storage = TableStorage((1, 10), encoder)
+        self.policy = EGreedyPolicy(storage, random_action_rate)
         pass
 
     def _start(self, observation):
-        choice = self._action_space['choice']
-        action = {'choice': random.choice(range(choice.min, choice.max))}
+        #encoded_observation = observation['state']
+        import pdb; pdb.set_trace()
+        #encoded_observation = self.encoder['observation'].encode(observation)
+        #encoded_action = self.policy.select_action(encoded_observation)
+        ##action = {'choice': encoded_action}
+        #action = self.encoder['action'].decode(encoded_action)
+        action = self.policy.select_action(observation)
         return action
+        #choice = self._action_space['choice']
+        #action = {'choice': random.choice(range(choice.min, choice.max))}
+        #return action
 
     def _step(self, reward, observation):
-        choice = self._action_space['choice']
-        action = {'choice': random.choice(range(choice.min, choice.max))}
+        ##encoded_observation = observation['state']
+        #encoded_observation = self.encoder['observation'].encode(observation)
+        #encoded_action = self.policy.select_action(encoded_observation)
+        ##action = {'choice': encoded_action}
+        #action = self.encoder['action'].decode(encoded_action)
+        action = self.policy.select_action(observation)
         return action
+        #choice = self._action_space['choice']
+        #action = {'choice': random.choice(range(choice.min, choice.max))}
+        #return action
 
 
 class ActionValueEnvironment(Environment):
-    actions_spec = {'choice': Integer(0, 10)}
+    actions_spec = {'choice': Integer(0, 9)}
     observations_spec = {'state': Integer(0, 0)}
     problem_type = "episodic"
     discount_factor = 1.0
@@ -39,7 +68,7 @@ class ActionValueEnvironment(Environment):
 
     def _init(self):
         maxval = self._action_space["choice"].max
-        self.ps = [ p/float(maxval) for p in range(maxval) ]
+        self.ps = [ p/float(maxval) for p in range(maxval+1) ]
 
     def _start(self):
         return dict(state=0)
