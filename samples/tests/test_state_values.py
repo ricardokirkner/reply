db62a65f1f412aced56e9d81d8f62f439f78677e
lucio.torre <lucio.torre@localhost>
2009-06-20 01:25:03 -0300

    add test for state value

--- samples/tests/test_state_values.py
+++ samples/tests/test_state_values.py
@@ -3,15 +3,15 @@ import random
 
 from reply.runner import Run
 from reply.experiment import Experiment
-from .samples.action_value import ActionValueAgent, ActionValueEnvironment
+from .samples.state_value import StateValueAgent, StateValueEnvironment
 
 class TestActionValue(unittest.TestCase):
     def test_run(self):
         # use fixed sequence for random
         random.seed(1)
 
-        agent = ActionValueAgent()
-        env = ActionValueEnvironment()
+        agent = StateValueAgent()
+        env = StateValueEnvironment()
         outterself = self
 
         class TestExperiment(Experiment):
@@ -20,9 +20,10 @@ class TestActionValue(unittest.TestCase):
                 for i in xrange(10000):
                     self.episode()
                 self.cleanup()
+                data = agent.learner.policy.storage.data.transpose()
                 error = sum(sum(abs(
-                    env.ps - agent.learner.policy.storage.data)))
-                outterself.assert_(error < 0.13, "Error too big: %s"%(error))
+                    env.ps - data)))
+                outterself.assert_(error < 0.14, "Error too big: %s"%(error))
 
         r = Run()
         r.run(agent, env, TestExperiment())
