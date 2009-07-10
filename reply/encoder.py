760d9ffd5d9576d1782531439fbe665b95d70696
ricardokirkner <ricardokirkner@localhost>
2009-07-09 21:46:17 -0300

    added BucketEncoder for discretizing continuous parameters

--- reply/encoder.py
+++ reply/encoder.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 
 class Encoder(object):
     def encode(self, item):
@@ -12,6 +13,67 @@ class DummyEncoder(Encoder):
         return item
     def decode(self, item):
         return item
+
+
+class BucketEncoder(Encoder):
+    def __init__(self, param, num_buckets=2):
+        self._min = param.min
+        self._max = param.max
+        self._num_buckets = num_buckets
+        self.buckets = self._generate_buckets()
+
+    @apply
+    def min():
+        def fget(self):
+            return self._min
+        def fset(self, value):
+            self._min = value
+            self.buckets = self._generate_buckets()
+        return property(fget, fset)
+
+    @apply
+    def max():
+        def fget(self):
+            return self._max
+        def fset(self, value):
+            self._max = value
+            self.buckets = self._generate_buckets()
+        return property(fget, fset)
+
+    @apply
+    def num_buckets():
+        def fget(self):
+            return self._num_buckets
+        def fset(self, value):
+            self._num_buckets = value
+            self.buckets = self._generate_buckets()
+        return property(fget, fset)
+
+    def _generate_buckets(self):
+        step = Decimal(str((self.max-self.min)/float(self.num_buckets)))
+        next = self.min
+        buckets = []
+        while next <= self.max:
+            buckets.append(next)
+            next += step
+        return buckets
+
+    def encode(self, item):
+        if item > self.max or item < self.min:
+            raise ValueError("item (%f) outside of encoding range" % item)
+
+        item = Decimal(str(item))
+        for bucket, bucket_start in enumerate(self.buckets[:-1]):
+            if item >= bucket_start and item < self.buckets[bucket+1]:
+                break
+        return bucket
+
+    def decode(self, encoded_item):
+        if encoded_item < 0 or encoded_item > self.num_buckets-1:
+            raise ValueError("item (%d) is not a valid encoding" % encoded_item)
+
+        bucket_start = self.buckets[encoded_item]
+        return float(bucket_start)
 
 
 class SpaceEncoder(Encoder):
@@ -54,6 +116,32 @@ class SpaceEncoder(Encoder):
         {'choice': 2}
         """
         return self.decode((action_id,))
+
+
+class CompoundSpaceEncoder(SpaceEncoder):
+    def __init__(self, space, encoders=None):
+        super(CompoundSpaceEncoder, self).__init__(space)
+        self.encoders = encoders if encoders is not None else {}
+
+    def encode(self, item):
+        encoded_item = []
+        for key in self.space.get_names_list():
+            if key in self.encoders:
+                encoded_key = self.encoders[key].encode(item[key])
+            else:
+                encoded_key = item[key]
+            encoded_item.append(encoded_key)
+        return tuple(encoded_item)
+
+    def decode(self, encoded_item):
+        item = {}
+        for key, encoded_value in zip(self.space.get_names_list(), encoded_item):
+            if key in self.encoders:
+                value = self.encoders[key].decode(encoded_value)
+            else:
+                value = encoded_value
+            item[key] = value
+        return item
 
 
 class StateActionEncoder(SpaceEncoder):
--- reply/tests/test_encoder.py
+++ reply/tests/test_encoder.py
@@ -1,7 +1,8 @@
 import unittest
 
-from reply.datatypes import Integer, Space
-from reply.encoder import Encoder, DummyEncoder, SpaceEncoder, StateActionEncoder
+from reply.datatypes import Double, Integer, Space
+from reply.encoder import BucketEncoder, DummyEncoder, Encoder, SpaceEncoder
+from reply.encoder import CompoundSpaceEncoder, StateActionEncoder
 
 
 class TestEncoder(unittest.TestCase):
@@ -28,6 +29,62 @@ class TestDummyEncoder(unittest.TestCase):
         self.assertEqual(self.encoder.decode(value), value)
 
 
+class TestBucketEncoder(unittest.TestCase):
+    def setUp(self):
+        self.param = Double(0, 1)
+        self.encoder = BucketEncoder(self.param)
+
+    def test_encode(self):
+        encoded_item = self.encoder.encode(self.param.min)
+        self.assertEqual(encoded_item, 0)
+        encoded_item = self.encoder.encode(self.param.max)
+        self.assertEqual(encoded_item, 1)
+
+        encoded_item = self.encoder.encode(0.49)
+        self.assertEqual(encoded_item, 0)
+        encoded_item = self.encoder.encode(0.5)
+        self.assertEqual(encoded_item, 1)
+
+    def test_encode_buckets(self):
+        self.encoder.num_buckets = 10
+
+        encoded_item = self.encoder.encode(self.param.min)
+        self.assertEqual(encoded_item, 0)
+        encoded_item = self.encoder.encode(self.param.max)
+        self.assertEqual(encoded_item, 9)
+
+        encoded_item = self.encoder.encode(0.3)
+        self.assertEqual(encoded_item, 3)
+        encoded_item = self.encoder.encode(0.73)
+        self.assertEqual(encoded_item, 7)
+
+        self.encoder.num_buckets = 5
+        encoded_item = self.encoder.encode(0.27)
+        self.assertEqual(encoded_item, 1)
+        encoded_item = self.encoder.encode(0.19999999999)
+        self.assertEqual(encoded_item, 0)
+
+    def test_decode(self):
+        item = self.encoder.decode(0)
+        self.assertEqual(item, self.param.min)
+        item = self.encoder.decode(1)
+        self.assertEqual(item, 0.5)
+
+        self.encoder.num_buckets = 10
+
+        item = self.encoder.decode(3)
+        self.assertEqual(item, 0.3)
+        item = self.encoder.decode(7)
+        self.assertEqual(item, 0.7)
+
+        self.encoder.num_buckets = 4
+
+        item = self.encoder.decode(2)
+        self.assertEqual(item, 0.5)
+        item = self.encoder.decode(3)
+        self.assertEqual(item, 0.75)
+
+
 class TestSpaceEncoder(unittest.TestCase):
     def setUp(self):
         space = Space({'choice': Integer(0, 9)})
@@ -43,6 +100,33 @@ class TestSpaceEncoder(unittest.TestCase):
         encoded_item = (5,)
         item = self.encoder.decode(encoded_item)
         expected_item = {'choice': 5}
+        self.assertEqual(item, expected_item)
+
+
+class TestCompoundSpaceEncoder(unittest.TestCase):
+    def setUp(self):
+        space = Space({'choice': Double(0, 1)})
+        encoders = {'choice': BucketEncoder(space.choice)}
+        self.encoder = CompoundSpaceEncoder(space, encoders)
+
+    def test_encode(self):
+        item = {'choice': 0.34}
+        encoded_item = self.encoder.encode(item)
+        expected_encoded_item = (0,)
+        self.assertEqual(encoded_item, expected_encoded_item)
+        item = {'choice': 0.82}
+        encoded_item = self.encoder.encode(item)
+        expected_encoded_item = (1,)
+        self.assertEqual(encoded_item, expected_encoded_item)
+
+    def test_decode(self):
+        encoded_item = (0,)
+        item = self.encoder.decode(encoded_item)
+        expected_item = {'choice': 0.0}
+        self.assertEqual(item, expected_item)
+        encoded_item = (1,)
+        item = self.encoder.decode(encoded_item)
+        expected_item = {'choice': 0.5}
         self.assertEqual(item, expected_item)
 
 
