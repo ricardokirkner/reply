1af03af0af94c9b19d795324074d5097d5b1cbc0
ricardokirkner <ricardokirkner@localhost>
2009-07-09 21:48:18 -0300

    some tweaks and fixes in the pole_cart demo
    - changed to used compound space encoder to encode the continuous space
    to a discrete set of intervals
    
    still needs more work: make the StateActionEncoder transform a variable number
    of dimensions map onto just two

--- samples/pole_cart.py
+++ samples/pole_cart.py
@@ -4,39 +4,18 @@ import os
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 #
 
+import itertools
+import math
 import random
 
 from reply.agent import LearningAgent
 from reply.datatypes import Double, Integer, Model, Space
-from reply.encoder import SpaceEncoder
+from reply.encoder import BucketEncoder, CompoundSpaceEncoder
 from reply.environment import Environment
 from reply.experiment import Experiment
 from reply.learner import QLearner
 from reply.policy import EGreedyPolicy
 from reply.storage import TableStorage
-
-
-# Common model
-observations = Space(dict(
-        velocity=Double(-1, 1),
-        angle=Double(-90, 90),
-        angle_velocity=Double(-1, 1)))
-actions = Space({'force': Double(-1, 1)})
-PoleCartModel = Model(observations, actions)
-
-
-class PoleCartAgent(LearningAgent):
-    model = PoleCartModel
-    state_encoder_class = SpaceEncoder
-    action_encoder_class = SpaceEncoder
-    storage_class = TableStorage
-    policy_class = EGreedyPolicy
-    learner_class = QLearner
-
-    learning_rate = 1
-    learning_rate_decay = 0.999
-    learning_rate_min = 0.001
-    random_action_rate = 1
 
 
 GRAVITY = 9.8
@@ -49,6 +28,43 @@ FORCE_MAG = 10.0
 TAU = 0.02 # dt
 FOURTHIRDS = 1.3333333333333
 
+# Common model
+observations = Space(dict(
+        velocity=Double(-1, 1),
+        angle=Double(-90, 90),
+        angle_velocity=Double(-1, 1)))
+actions = Space({'force': Double(-1, 1)})
+PoleCartModel = Model(observations, actions)
+
+
+class PoleCartStateEncoder(CompoundSpaceEncoder):
+    def __init__(self, space):
+        encoders = {'velocity': BucketEncoder(observations.velocity, 10),
+                    'angle': BucketEncoder(observations.angle, 36),
+                    'angle_velocity': BucketEncoder(observations.angle_velocity, 10)}
+        super(PoleCartStateEncoder, self).__init__(space, encoders)
+
+
+class PoleCartActionEncoder(CompoundSpaceEncoder):
+    def __init__(self, space):
+        encoders = {'force': BucketEncoder(actions.force, 10)}
+        super(PoleCartActionEncoder, self).__init__(space, encoders)
+
+
+class PoleCartAgent(LearningAgent):
+    model = PoleCartModel
+    state_encoder_class = PoleCartStateEncoder
+    action_encoder_class = PoleCartActionEncoder
+    storage_class = TableStorage
+    policy_class = EGreedyPolicy
+    learner_class = QLearner
+
+    learning_rate = 1
+    learning_rate_decay = 0.999
+    learning_rate_min = 0.001
+    random_action_rate = 1
+
+
 class PoleCartEnvironment(Environment):
     problem_type = "episodic"
     discount_factor = 1.0
@@ -60,21 +76,22 @@ class PoleCartEnvironment(Environment):
         self.x_dot = 0
         self.theta = 0
         self.theta_dot = 0
+        return super(PoleCartEnvironment, self).init()
 
     def start(self):
         return self.get_state()
 
     def get_state(self):
-        return dict(velocity=self.x_dot, angle=self.theta,
+        return dict(velocity=self.x_dot, angle=math.degrees(self.theta),
                     angle_velocity=self.theta_dot)
 
     def step(self, action):
-        f = action['force']
+        force = action['force']
 
-        costheta = Math.cos(theta);
-        sintheta = Math.sin(theta);
+        costheta = math.cos(self.theta)
+        sintheta = math.sin(self.theta)
 
-        temp = ((force + POLEMASS_LENGTH * theta_dot * theta_dot * sintheta)
+        temp = ((force + POLEMASS_LENGTH * self.theta_dot * self.theta_dot * sintheta)
                              / TOTAL_MASS)
 
         thetaacc = ((GRAVITY * sintheta - costheta* temp)
@@ -85,28 +102,32 @@ class PoleCartEnvironment(Environment):
 
         # Update the four state variables, using Euler's method.
 
-        self.x  += TAU * self.x_dot;
-        self.x_dot += TAU * self.xacc;
-        self.theta += TAU * self.theta_dot;
-        self.theta_dot += TAU * self.thetaacc;
+        self.x  += TAU * self.x_dot
+        self.x_dot += TAU * xacc
+        self.theta += TAU * self.theta_dot
+        self.theta_dot += TAU * thetaacc
 
-        while self.theta > math.PI:
-            theta -= 2.0 * math.PI
+        while self.theta > math.pi:
+            self.theta -= 2.0 * math.pi
 
-        while theta < -math.PI:
-            theta += 2.0 * math.PI
+        while self.theta < -math.pi:
+            self.theta += 2.0 * math.pi
 
 
         r = 0
+        terminal = False
         if abs(math.degrees(self.theta)) > 80:
             terminal = True
             r = -1
         rot = self.get_state()
         rot.update(dict(reward=r, terminal=terminal))
+        import pdb; pdb.set_trace()
         return rot
+
 
 class PoleCartExperiment(Experiment):
     model = PoleCartModel
+
 
 if __name__=="__main__":
     from reply.runner import Runner
